9e1432892ca5947096b89de90b2ab17ee7886c1a
alejolopez-co <alejolopez.co@gmail.com>
2022-09-17 12:26:04 -0500

    Primer cambio con IC

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
-import HTML from './src/assets/html.png'    
-import CSS from './src/assets/css.png'
-import JavaScript from './src/assets/javascript.png'
-import ReactImg from './src/assets/react.png'
-import GitHub from './src/assets/githubWhite.png'
-import Java from './src/assets'
-import AzureSqlDatabase from './src/assets/azureSqlDatabase.jpg'
-import AzurePortal from './src/assets/azure.png' 
-import MySQL from './src/assets/mysql.png'
+import HTML from './src/assets/html.png';
+import CSS from './src/assets/css.png';
+import JavaScript from './src/assets/javascript.png';
+import ReactImg from './src/assets/react.png';
+import GitHub from './src/assets/githubWhite.png';
+import Java from './src/assets';
+import AzureSqlDatabase from './src/assets/azureSqlDatabase.jpg';
+import AzurePortal from './src/assets/azure.png';
+import MySQL from './src/assets/mysql.png';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div>
+
+    </div>
   )
 }
 
