b4b436f6c2bd9665de4657a710ba128b06693514
alejolopez-co <alejolopez.co@gmail.com>
2022-09-18 15:13:26 -0500

    Primera página de experiencia

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 
-import HTML from './src/assets/html.png';
-import CSS from './src/assets/css.png';
-import JavaScript from './src/assets/javascript.png';
-import ReactImg from './src/assets/react.png';
-import GitHub from './src/assets/githubWhite.png';
-import Java from './src/assets';
-import AzureSqlDatabase from './src/assets/azureSqlDatabase.jpg';
-import AzurePortal from './src/assets/azure.png';
-import MySQL from './src/assets/mysql.png';
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import GitHub from '../assets/githubWhite.png';
+import Java from '../assets/java.png';
+import AzureSqlDatabase from '../assets/azureSqlDatabase.jpg';
+import AzurePortal from '../assets/azure.png';
+import MySQL from '../assets/mysql.png';
 
 const Skills = () => {
   return (
     <div name='skills'>
-
+        {/* Container */}
+        <div>
+            <div>
+                <p>Experiencia</p>     
+            </div>
+        </div>
     </div>
   )
 }
