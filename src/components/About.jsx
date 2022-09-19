5b15f611bbe72c569b44b80e59cfe1a01c12a350
alejolopez-co <alejolopez.co@gmail.com>
2022-09-18 20:17:04 -0500

    Importar todos los iconos de tecnología

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,8 @@ export const About = () => {
     return (
         <div name='about' className='w-full h-screen bg-[#FFFFFE] text-[#2B2C34]'>
             <div className='flex flex-col justify-center items-center w-full h-full'>
-                <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
-                    <div className='sm:text-right pb-8 pl-4'>
+                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                    <div className='sm:text-right sm:pb-4'>
                         <p className='text-4xl font-bold inline border-b-4 border-[#6246EA]'>
                             Perfil
                         </p>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,33 +3,65 @@ import React from 'react'
 import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
-import ReactImg from '../assets/react.png';
+import ReactImg from '../assets/react.svg';
 import GitHub from '../assets/githubWhite.png';
 import Java from '../assets/java.png';
-import AzureSqlDatabase from '../assets/azureSqlDatabase.jpg';
+import AzureSqlDatabase from '../assets/azureSqlDatabase.svg';
 import AzurePortal from '../assets/azure.png';
 import MySQL from '../assets/mysql.png';
 
 const Skills = () => {
-  return (
-    <div name='skills'>
-        {/* Container */}
-        <div>
-            <div>
-                <p>Experiencia</p>
-                <p>Estos son los trabajos que he desempeñado:</p> 
-            </div>
-
-            <div>
+    return (
+        <div name='skills' className='bg-[#FFFFFE] text-[#2B2C34]'>
+            {/* Container */}
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
-                    <img src={HTML} alt="HTML icon" />
-                    <p>HTML</p>
+                    <p className='text-4xl font-bold inline border-b-4 border-[#6246EA]'>
+                        Experiencia
+                    </p>
+                    <p className='py-4'>
+                        // Estas son las tecnologías con las que he trabajado:
+                    </p>
                 </div>
+
+                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 text-black'>
+                    <div className='shadow-md shadow-[#D1D1E9] hover:scale-110 duration-100'>
+                        <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+                        <p className=''>HTML</p>
+                    </div>
+                    <div className='shadow-md shadow-[#D1D1E9] hover:scale-110 duration-100'>
+                        <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+                        <p className=''>CSS</p>
+                    </div>
+                    <div className='shadow-md shadow-[#D1D1E9] hover:scale-110 duration-100'>
+                        <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript icon" />
+                        <p className=''>JavaScript</p>
+                    </div>
+                    <div className='shadow-md shadow-[#D1D1E9] hover:scale-110 duration-100'>
+                        <img className='w-16 h-20 mx-auto' src={ReactImg} alt="React icon" />
+                        <p className=''>React</p>
+                    </div>
+                    <div className='shadow-md shadow-[#D1D1E9] hover:scale-110 duration-100'>
+                        <img className='w-20 mx-auto' src={GitHub} alt="GitHub icon" />
+                        <p className=''>GitHub</p>
+                    </div>
+                    <div className='shadow-md shadow-[#D1D1E9] hover:scale-110 duration-100'>
+                        <img className='w-20 mx-auto' src={Java} alt="Java icon" />
+                        <p className=''>Java</p>
+                    </div>
+                    <div className='shadow-md shadow-[#D1D1E9] hover:scale-110 duration-100'>
+                        <img className='w-16 h-20 mx-auto' src={AzureSqlDatabase} alt="Azure Sql Database icon" />
+                        <p className=''>Sql database</p>
+                    </div>
+                    <div className='shadow-md shadow-[#D1D1E9] hover:scale-110 duration-100'>
+                        <img className='w-16 h-20 mx-auto' src={AzurePortal} alt="Azure Portal icon" />
+                        <p className=''>Azure portal</p>
+                    </div>
+                </div>
+
             </div>
-            
         </div>
-    </div>
-  )
+    )
 }
 
 export default Skills
