d8dcfc95858b8d40256a2532f743dcfe9c729a66
alejolopez-co <alejolopez.co@gmail.com>
2022-09-26 09:33:07 -0500

    Se agregaron mas competencias al Perfil

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -58,7 +58,7 @@ export const About = () => {
                             {/* Icon */}
                             <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
                             <span className="font-light leading-tight">
-                                Experto en sistemas de Ayudas Diagnósticas
+                                Experto en sistemas de Ayudas Diagnósticas e integración con dispositivos biomédicos
                             </span>
                         </li>
                         <li className="flex space-x-2">
@@ -90,6 +90,54 @@ export const About = () => {
                             <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
                             <span className="font-light leading-tight">
                                 Monitoreo Dynatrace, solución de incidentes
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <div className='sm:text-right text-lg font-bold'>
+                    <p>
+                        Programación
+                    </p>
+                </div>
+                <div className='font-medium'>
+                    {/* List */}
+                    <ul role="list" className="space-y-4 text-gray-500 dark:text-gray-400">
+                        <li className="flex space-x-2">
+                            {/* Icon */}
+                            <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
+                            <span className="font-light leading-tight">
+                                Javascript, HTML5, CSS, React
+                            </span>
+                        </li>
+                        <li className="flex space-x-2">
+                            {/* Icon */}
+                            <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
+                            <span className="font-light leading-tight">
+                                Conocimientos en Java, Spring Boot, PHP, PowerBuilder
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <div className='sm:text-right text-lg font-bold'>
+                    <p>
+                        Conocimientos en BD
+                    </p>
+                </div>
+                <div className='font-medium'>
+                    {/* List */}
+                    <ul role="list" className="space-y-4 text-gray-500 dark:text-gray-400">
+                        <li className="flex space-x-2">
+                            {/* Icon */}
+                            <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
+                            <span className="font-light leading-tight">
+                                Experto en implementación de BD SQL Server
+                            </span>
+                        </li>
+                        <li className="flex space-x-2">
+                            {/* Icon */}
+                            <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
+                            <span className="font-light leading-tight">
+                                Tuning de BD SQL Server
                             </span>
                         </li>
                     </ul>
